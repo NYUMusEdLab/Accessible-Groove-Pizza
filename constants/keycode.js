cf362363deb3467734dd7083e16f06e35ddfa1d2
Hongwei (Henry) Zhou <hz1101@nyu.edu>
2019-03-20 22:06:04 -0400

    two keymaps added

---
+++ constants/keycode.js
@@ -0,0 +1,42 @@
+const KEY_DIGIT1 = 49
+const KEY_DIGIT2 = 50
+const KEY_DIGIT3 = 51
+const KEY_DIGIT4 = 52
+
+const KEY_Q = 81
+const KEY_W = 87
+const KEY_E = 69
+const KEY_R = 82
+const KEY_T = 84
+const KEY_Y = 89
+const KEY_U = 85
+const KEY_I = 73
+
+const KEY_A = 65
+const KEY_S = 83
+const KEY_D = 68
+const KEY_F = 70
+const KEY_G = 71
+const KEY_H = 72
+const KEY_J = 74
+const KEY_K = 75
+
+const KEY_Z = 90
+const KEY_X = 88
+const KEY_C = 67
+const KEY_V = 86
+const KEY_B = 66
+const KEY_N = 78
+
+const KEY_MINUS = 189
+const KEY_EQUAL = 187
+const KEY_SPACE = 32
+const KEY_ENTER = 13
+const KEY_ALTLEFT = 18
+const KEY_CTRLLEFT = 17
+const KEY_SHIFTLEFT = 16
+
+const KEY_PAGEUP = 33
+const KEY_PAGEDOWN = 34
+
+const KEY_BACKSLASH = 200
