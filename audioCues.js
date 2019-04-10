6620e405745131223c1f306b8048fb41948268c6
Alex Yixuan Xu <yx1137@nyu.edu>
2019-04-10 11:33:26 -0400

    simplify audio

--- audioCues.js
+++ audioCues.js
@@ -6,10 +6,19 @@
 
 let synth = new Tone.Synth().toMaster();
 synth.oscillator.type = 'triangle';
+synth.volume.value = -6;
+
+// for testing purposes...
+/*
+synth.envelope.attack = 0.09;
+synth.envelope.decay = 0.0795;
+synth.envelope.sustain = 0;
+synth.envelope.release = 0.02;
+*/
 
 /*
 {
-	oscillator  : {
+oscillator  : {
 	type  : triangle
 	}  ,
 	envelope  : {
@@ -20,13 +29,16 @@ synth.oscillator.type = 'triangle';
 	}
 }
 */
-let startMidi = 60;
-let noteArr = [];
-for (let i=0; i<16; i++){
-	let currentMidi = startMidi+i*2;
-	let currentNote = Tone.Frequency(currentMidi, "midi").toNote();
-	noteArr.push(currentNote);
-}
+
+let noteArr = ['E3', 'F3', 'G3', 'A3', 'B3',
+			   'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4',
+			   'C5', 'D5', 'E5', 'F5'];
+		/*	   
+let noteArr = [
+			   'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4',
+			   'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5',
+			   'C6', 'D6'];
+			   */
 console.log(noteArr);
 
 function playSliceAudio(slice){
